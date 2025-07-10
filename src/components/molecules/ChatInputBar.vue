2f0670f3961e2c015bb45cec783b89729fe7969e
stella317 <stella.se.baek@gmail.com>
2025-07-10 10:57:59 +0900

    feat: 채팅 화면에 뒤로가기, 설정 버튼 추가

--- src/components/molecules/ChatInputBar.vue
+++ src/components/molecules/ChatInputBar.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="chat-input">
     <input type="text" placeholder="메시지를 입력해주세요" />
-    <button>--</button>
+    <button class="send-button">
+      <font-awesome-icon :icon="['fas', 'paper-plane']" />
+    </button>
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,37 @@
 import '@/assets/styles/main.scss'
 
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faEye, faCircleCheck, faUser, faLock } from '@fortawesome/free-solid-svg-icons'         // solid
-import { faEye as faEyeRegular, faCircleCheck as faCircleCheckRegular} from '@fortawesome/free-regular-svg-icons' // regular
+import {
+  faEye,
+  faCircleCheck,
+  faUser,
+  faLock,
+  faArrowLeft,
+  faEllipsisVertical,
+  faPaperPlane,
+} from '@fortawesome/free-solid-svg-icons' // solid
+import {
+  faEye as faEyeRegular,
+  faCircleCheck as faCircleCheckRegular,
+} from '@fortawesome/free-regular-svg-icons' // regular
 
-library.add(faEye, faEyeRegular, faUser, faLock, faCircleCheck, faCircleCheckRegular)
+library.add(
+  faEye,
+  faEyeRegular,
+  faUser,
+  faLock,
+  faCircleCheck,
+  faCircleCheckRegular,
+  faArrowLeft,
+  faEllipsisVertical,
+  faPaperPlane,
+)
 
 import App from './App.vue'
 import router from './router'
-
 
 const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
