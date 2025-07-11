410ae9cd6e331f96538d4906ee6b6256e4ce2e2c
sangeun-b <baek0007@algonquinlive.com>
2025-07-11 23:08:42 +0900

    style: 댓글 입력창 placeholder 내용 변경

--- src/components/molecules/CommentInputBar.vue
+++ src/components/molecules/CommentInputBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="comment-input">
-    <input type="text" placeholder="메시지를 입력해주세요" />
+    <input type="text" placeholder="댓글을 입력해주세요" />
     <button class="send-button">
       <font-awesome-icon :icon="['fas', 'arrow-right']" />
     </button>
