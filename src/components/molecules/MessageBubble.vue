55a4342166197fa015ba8c929085e5d511aac035
stella317 <stella.se.baek@gmail.com>
2025-07-15 11:06:15 +0900

    refactor: messagebubble 객체형 props으로 변경

--- src/components/molecules/MessageBubble.vue
+++ src/components/molecules/MessageBubble.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="message-row" :class="isMine ? 'mine' : 'other'">
-    <div v-if="!isMine" class="profile-icon"></div>
+  <div class="message-row" :class="message.isMine ? 'mine' : 'other'">
+    <div v-if="!message.isMine" class="profile-icon"></div>
     <div class="message-bubble">
-      <p class="message-text">{{ text }}</p>
-      <span class="message-time">{{ time }}</span>
+      <p class="message-text">{{ message.text }}</p>
+      <span class="message-time">{{ message.time }}</span>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 defineProps<{
-  text: string
-  time: string
-  isMine: boolean
+  message: {
+    text: string
+    time: string
+    isMine: boolean
+  }
 }>()
 </script>
--- src/features/chat/pages/ChatView.vue
+++ src/features/chat/pages/ChatView.vue
@@ -3,26 +3,7 @@
     <div class="chat-date">2026년 12월 25일 금요일</div>
     <div class="chat-body">
       <section class="messages">
-        <MessageBubble
-          text="안녕하세요! 사용하시는 화장품이 뭔지 궁금해요!"
-          time="오후 3:45"
-          :isMine="false"
-        />
-        <MessageBubble
-          text="제가 사용하는 화장품을 알려드릴게요!"
-          time="오후 4:23"
-          :isMine="true"
-        />
-        <MessageBubble
-          text="어떤 화장품이 주로 알고 싶으신지 궁금하네요!!!"
-          time="오후 4:23"
-          :isMine="true"
-        />
-        <MessageBubble
-          text="감사합니다. 알려주시면 그 화장품 그대로 똑같이 구매할 생각이에요!!!!!!"
-          time="오후 5:45"
-          :isMine="false"
-        />
+        <MessageBubble v-for="(msg, i) in messages" :key="i" :message="msg" />
       </section>
     </div>
     <ChatInputBar />
@@ -32,4 +13,16 @@
 <script setup lang="ts">
 import ChatInputBar from '@/components/molecules/ChatInputBar.vue'
 import MessageBubble from '@/components/molecules/MessageBubble.vue'
+
+interface Message {
+  text: string
+  time: string
+  isMine: boolean
+}
+const messages: Message[] = [
+  { text: '안녕하세요!', time: '오후 3:45', isMine: false },
+  { text: '제가 사용하는 화장품을 알려드릴게요!', time: '오후 4:23', isMine: true },
+  { text: '어떤 화장품이 궁금하신가요?', time: '오후 4:23', isMine: true },
+  { text: '감사합니다! 그대로 구매할게요!', time: '오후 5:45', isMine: false },
+]
 </script>
