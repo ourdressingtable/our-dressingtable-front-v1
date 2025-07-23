a63a8bdc22b7ee2ff5d9353a25ebd6d8fdf38c85
stella317 <stella.se.baek@gmail.com>
2025-07-23 14:28:53 +0900

    feat: 알림 목록 화면 추가

---
+++ src/components/icons/IconNewMark.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    width="1em"
+    height="1em"
+  >
+    <path
+      d="M0 10C0 4.47715 4.47715 0 10 0C15.5228 0 20 4.47715 20 10C20 15.5228 15.5228 20 10 20C4.47715 20 0 15.5228 0 10Z"
+      fill="currentColor"
+    />
+    <path
+      d="M14.7443 3.86364V15.5H12.6193L7.55682 8.17614H7.47159V15.5H5.01136V3.86364H7.17045L12.1932 11.1818H12.2955V3.86364H14.7443Z"
+      fill="white"
+    />
+  </svg>
+</template>
--- src/features/notification/pages/NotificationView.vue
+++ src/features/notification/pages/NotificationView.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="notification-list-main">
-    <div class="notification-list-wrapper"></div>
+    <section class="noticiation-list">
+      <NotificationItem
+        v-for="(item, index) in notifications"
+        :key="index"
+        :date="item.date"
+        :time="item.time"
+        :content="item.content"
+        :isNew="item.isNew"
+      />
+    </section>
   </div>
 </template>
+
+<script setup lang="ts">
+import NotificationItem from '@/components/molecules/NotificationItem.vue'
+
+interface Notification {
+  date: string
+  time: string
+  content: string
+  isNew: boolean
+}
+
+const notifications: Notification[] = [
+  { date: '2026.12.24', time: '09:00', content: '앰플 사용기한이 30일 남았습니다.', isNew: true },
+  { date: '2026.12.20', time: '09:00', content: '크림 사용기한이 10일 남았습니다.', isNew: false },
+]
+</script>
