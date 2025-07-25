b904403017230f7a89dfda5662a131e10e07727b
stella317 <stella.se.baek@gmail.com>
2025-07-25 15:23:43 +0900

    feat: 화장품 등록 폼 TYPE 생성

---
+++ src/type/CosmeticForm.ts
@@ -0,0 +1,15 @@
+export interface CosmeticForm {
+  image: File | null
+  brand: string
+  name: string
+  category: string
+  expirationDate: string
+  openDate: string
+  usagePeriodAfterOpen: string
+  price: number
+  shop: string
+  alarmEnabled: boolean
+  alarmDate: string
+  customUsageDate: string
+  finalExpirationDate: string
+}
