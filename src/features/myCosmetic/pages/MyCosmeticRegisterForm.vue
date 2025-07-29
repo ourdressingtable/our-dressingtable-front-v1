9ada9fe723264260f14eaa1a0670eb2d21415975
stella317 <stella.se.baek@gmail.com>
2025-07-29 16:33:00 +0900

    feat: 화장품 등록 페이지 드롭 다운 스타일 수정

--- src/features/myCosmetic/pages/MyCosmeticRegisterForm.vue
+++ src/features/myCosmetic/pages/MyCosmeticRegisterForm.vue
@@ -5,7 +5,22 @@
     <BaseInput v-model="form.name" label="제품명" placeholder="제품명을 입력하세요" />
     <p v-if="errors.name" class="input-error">{{ errors.name }}</p>
     <!-- <BaseSelect v-model="form.category" label="카테고리" :options="categoryOptions" /> -->
-    <BaseCategorySelector v-model="form.category" label="카테고리" />
+    <!-- <BaseCategorySelector v-model="form.category" label="카테고리" /> -->
+
+    <!-- 카테고리 선택 필드 -->
+    <div class="category-input-wrapper" @click="showCategoryModal = true">
+      <label class="category-label" :class="{ active: form.categoryLabel }">카테고리</label>
+      <div class="category-input-value">
+        {{ form.categoryLabel || '' }}
+      </div>
+    </div>
+
+    <CosmeticCategoryModal
+      v-model="form.categoryValue"
+      v-model:show="showCategoryModal"
+      :category-tree="categoryTree"
+      @select:labelAndValue="onSelectCategory"
+    />
     <BaseDatePicker
       v-model="form.expirationDate"
       label="유통기한"
@@ -22,7 +37,6 @@
     <BaseSelect
       v-model="form.usagePeriodAfterOpen"
       label="오픈 후 사용 기한"
-      placeholder="오픈 후 사용 기한"
       :options="usagePeriodOptions"
     />
     <BaseDatePicker
@@ -80,13 +94,15 @@ import BaseSelect from '@/components/atoms/BaseSelect.vue'
 import BaseToggle from '@/components/atoms/BaseToggle.vue'
 import ImageUploader from '@/components/atoms/ImageUploader.vue'
 import PriceInput from '@/components/atoms/PriceInput.vue'
-import BaseCategorySelector from '@/components/atoms/BaseCategorySelector.vue'
+// import BaseCategorySelector from '@/components/atoms/BaseCategorySelector.vue'
+import CosmeticCategoryModal from '@/components/modals/CosmeticCategoryModal.vue'
 
 const form = ref<CosmeticForm>({
   image: null,
   brand: '',
   name: '',
-  category: '',
+  categoryLabel: '',
+  categoryValue: '',
   expirationDate: '',
   openDate: '',
   usagePeriodAfterOpen: '',
@@ -95,8 +111,43 @@ const form = ref<CosmeticForm>({
   alarmEnabled: false,
   alarmDate: '30',
   customUsageDate: '',
-  finalExpirationDate: '',
 })
+
+const categoryTree = [
+  {
+    label: '스킨케어',
+    value: 'skincare',
+    children: [
+      {
+        label: '크림',
+        value: 'cream',
+        children: [
+          { label: '수분크림', value: 'moisture_cream' },
+          { label: '재생크림', value: 'repair_cream' },
+        ],
+      },
+      {
+        label: '로션',
+        value: 'lotion',
+        children: [{ label: '미백로션', value: 'whitening_lotion' }],
+      },
+    ],
+  },
+  {
+    label: '메이크업',
+    value: 'makeup',
+    children: [
+      {
+        label: '립',
+        value: 'lip',
+        children: [
+          { label: '립스틱', value: 'lipstick' },
+          { label: '틴트', value: 'tint' },
+        ],
+      },
+    ],
+  },
+]
 
 const usagePeriodOptions = [
   { label: '1개월', value: '1M' },
@@ -111,6 +162,8 @@ const alarmDateOptions = [
   { label: 'D-Day', value: '0' },
 ]
 
+const showCategoryModal = ref(false)
+
 const showCustomDateField = computed(() => {
   return form.value.usagePeriodAfterOpen === 'custom'
 })
@@ -119,40 +172,12 @@ const { finalExpirationDate } = useFinalExpirationDate(form)
 
 const { errors, validate } = useFormValidation(form)
 
-// const errors = reactive<Record<string, string>>({})
-// const validateForm = () => {
-//   errors.name = form.value.name.trim() === '' ? '제품명을 입력해주세요.' : ''
-
-//   if (isNaN(form.value.price) || form.value.price < 0) {
-//     errors.price = '가격은 숫자로 입력해주세요.'
-//   } else {
-//     errors.price = ''
-//   }
-
-//   const isDate = (str: string) => /^\d{4}-\d{2}-\d{2}$/.test(str)
-
-//   errors.openDate =
-//     form.value.openDate && !isDate(form.value.openDate) ? '날짜 형식은 YYYY-MM-DD여야 합니다.' : ''
-
-//   errors.expirationDate =
-//     form.value.expirationDate && !isDate(form.value.expirationDate)
-//       ? '날짜 형식은 YYYY-MM-DD여야 합니다.'
-//       : ''
-
-//   if (form.value.usagePeriodAfterOpen === 'custom') {
-//     errors.customUsageDate =
-//       form.value.customUsageDate && !isDate(form.value.customUsageDate)
-//         ? '직접 입력 날짜 형식(YYYY-MM-DD)이 올바르지 않습니다.'
-//         : ''
-//   } else {
-//     errors.customUsageDate = ''
-//   }
-
-//   return Object.values(errors).every((v) => v === '')
-// }
+function onSelectCategory({ label, value }: { label: string; value: string }) {
+  form.value.categoryLabel = label
+  form.value.categoryValue = value
+}
 
 const submitForm = () => {
-  // if (!validateForm()) return
   if (!validate()) return
   console.log('폼 전송:', form.value)
   // TODO: API 연동
