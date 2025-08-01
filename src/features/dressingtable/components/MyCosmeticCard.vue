c6451b44c6a4415ef9b8ba58c9efcc9fe16d9438
stella317 <stella.se.baek@gmail.com>
2025-08-01 10:42:55 +0900

    feat: 나의 화장품 컴포넌트 마크업 추가

---
+++ src/features/dressingtable/components/MyCosmeticCard.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="my-cosmetic-card">
+    <div class="my-cosmetic-card__image">
+      <span class="my-cosmetic-card__brand">{{ cosmetic.brand }}</span>
+      <img v-if="cosmetic.image" :src="cosmetic.image" :alt="cosmetic.name" />
+    </div>
+    <div class="my-cosmetic-card__name">
+      {{ cosmetic.name }}
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps<{
+  cosmetic: {
+    id: number
+    image?: string
+    name: string
+    brand: string
+  }
+}>()
+</script>
+<style scoped lang="scss">
+.my-cosmetic-card {
+  width: 100%;
+  aspect-ratio: 3 / 4;
+  border-radius: 4px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: white;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.03);
+  border: 1px solid #f9cdd1;
+
+  .my-cosmetic-card__image {
+    position: relative;
+    flex: 1;
+    background-color: #fbe4e6;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    // img {
+    //   width: 50%;
+    //   height: 50%;
+    //   object-fit: cover;
+    // }
+
+    .brand-label {
+      position: absolute;
+      top: 8px;
+      left: 8px;
+      background-color: #fff6f7;
+      color: #444;
+      font-size: 12px;
+      padding: 2px 8px;
+      border-radius: 4px;
+      font-weight: 500;
+      box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
+    }
+  }
+
+  .name {
+    padding: 10px;
+    font-size: 15px;
+    text-align: center;
+    font-weight: 500;
+    color: #5a3a3a;
+  }
+}
+</style>
--- src/features/dressingtable/pages/MyDressingtableView.vue
+++ src/features/dressingtable/pages/MyDressingtableView.vue
@@ -5,12 +5,15 @@
       <BaseSelect :options="sortOptions" v-model="selectedSort" />
     </div>
 
-    <div class="cosmetic-grid"></div>
+    <div class="cosmetic-grid">
+      <MyCosmeticCard v-for="cosmetic in filteredList" :key="cosmetic.id" :cosmetic="cosmetic" />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import BaseSelect from '@/components/atoms/BaseSelect.vue'
+import MyCosmeticCard from '../components/MyCosmeticCard.vue'
 
 const selectedCategory = ref('전체')
 const selectedSort = ref('등록순')
@@ -28,16 +31,33 @@ const categoryOptions = [
   { label: '에센스', value: 'essence' },
 ]
 
-// const list = ref([
-//   { id: 1, name: '무기자차 선크림' },
-//   { id: 2, name: '레티놀 앰플' },
-//   { id: 3, name: '닥터지 크림' },
-//   { id: 4, name: '흔적 세럼' },
-// ])
+const list = ref([
+  { id: 1, name: '무기자차 선크림', brand: '닥터지' },
+  { id: 2, name: '레티놀 앰플', brand: '이미스프리' },
+  { id: 3, name: '수분 크림', brand: '아이오페' },
+  { id: 4, name: '흔적 세럼', brand: '아이소이' },
+])
 
-// const filteredList = computed(() => {
-//   list.value.filter(
-//     (item) => selectedCategory.value === '전체' || item.brand === selectedCategory.value,
-//   )
-// })
+const filteredList = computed(() =>
+  list.value.filter(
+    (item) => selectedCategory.value === '전체' || item.brand === selectedCategory.value,
+  ),
+)
 </script>
+<style scoped lang="scss">
+.dressingtable-main {
+  padding-top: 16px;
+
+  .cosmetic-grid {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 16px;
+    padding: 0 16px;
+  }
+}
+@media (min-width: 768px) {
+  .cosmetic-grid {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+</style>
